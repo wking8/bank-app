2e7a3691032a607101335436692707731a026198
Wyatt King <wyattking@Wyatts-MacBook-Pro.local>
2019-08-08 10:41:39 -0600

    commit

---
+++ __instructor-code__/server/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+
+const app = express()
+
+app.use(express.json())
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 10
+  }
+}))
+
+massive(CONNECTION_STRING).then(db => {
+  app.set('db', db)
+  app.listen(SERVER_PORT, () => console.log(`Listening on ${SERVER_PORT}`))
+})
